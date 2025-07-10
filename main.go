46028c19b3f6030cc69d15649013f28bb3ceae71
blakehulett7 <blake.hulett7@gmail.com>
2025-07-10 15:46:24 -0500

    it is instance!

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "index.html") })
 	router.HandleFunc("/sign-up", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "sign_up.html") })
-	router.HandleFunc("/new-game", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "new_game.html") })
+	router.HandleFunc("/new-instance", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "new_instance.html") })
 
 	server := &http.Server{
 		Addr:    ":8080",
