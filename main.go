8b335786a3b68d131aa98ce04e8f419d77add7a9
blakehulett7 <blake.hulett7@gmail.com>
2025-07-10 07:12:44 -0500

    snakes over kebabs!

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "index.html") })
-	router.HandleFunc("/new-game", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "new_game.html")
-	})
+	router.HandleFunc("/sign-up", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "sign_up.html") })
+	router.HandleFunc("/new-game", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "new_game.html") })
 
 	server := &http.Server{
 		Addr:    ":8080",
